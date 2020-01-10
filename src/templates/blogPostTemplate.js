28cf88854bf33768a99eb0ebbc54bdaf26e71e4a
Christian Amauger <christian.amauger@gmail.com>
2020-01-10 15:09:06 -0500

    template update

--- src/templates/blogPostTemplate.js
+++ src/templates/blogPostTemplate.js
@@ -1,10 +1,10 @@
-import { graphql, Link } from 'gatsby';
-import { MDXRenderer } from 'gatsby-plugin-mdx';
-import React from 'react';
-import SEO from 'react-seo-component';
-import { Layout } from '../components/Layout';
-import Helmet from 'react-helmet'
-import { useSiteMetadata } from '../hooks/useSiteMetadata';
+import { graphql, Link } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import React from "react";
+import SEO from "react-seo-component";
+import { Layout } from "../components/Layout";
+import Helmet from "react-helmet";
+import { useSiteMetadata } from "../hooks/useSiteMetadata";
 
 export default ({ data, pageContext }) => {
   const {
@@ -13,21 +13,18 @@ export default ({ data, pageContext }) => {
     siteLanguage,
     siteLocale,
     twitterUsername,
-    authorName,
+    authorName
   } = useSiteMetadata();
   const { frontmatter, body, fields, excerpt } = data.mdx;
   const { title, date, cover } = frontmatter;
   const { previous, next } = pageContext;
   return (
     <Layout>
-     
       <SEO
         title={title}
         description={excerpt}
         image={
-          cover === null
-            ? `${siteUrl}${image}`
-            : `${siteUrl}${cover.publicURL}`
+          cover === null ? `${siteUrl}${image}` : `${siteUrl}${cover.publicURL}`
         }
         pathname={`${siteUrl}${fields.slug}`}
         siteLanguage={siteLanguage}
@@ -38,18 +35,18 @@ export default ({ data, pageContext }) => {
         publishedDate={date}
         modifiedDate={new Date(Date.now()).toISOString()}
       />
-  <Helmet>
-  <meta property="og:url" content={`${siteUrl}${fields.slug}`} />
-            <meta property="og:type" content="article" />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={excerpt} />
-            <meta property="og:image" content={image} />
-<meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:creator" content={twitterUsername} />
-            <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={excerpt} />
-            <meta name="twitter:image" content={image} />
-</Helmet>
+      <Helmet>
+        <meta property="og:url" content={`${siteUrl}${fields.slug}`} />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={excerpt} />
+        <meta property="og:image" content={image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content={twitterUsername} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={excerpt} />
+        <meta name="twitter:image" content={image} />
+      </Helmet>
 
       <h1>{frontmatter.title}</h1>
       <p>{frontmatter.date}</p>
