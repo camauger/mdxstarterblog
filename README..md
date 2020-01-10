1d87cf17058a85b386c9d43190af2daee3edc6fc
Christian Amauger <christian.amauger@gmail.com>
2020-01-10 12:41:22 -0500

    update to core-js@3

--- README..md
+++ README..md
@@ -1,3 +1,9 @@
 # MDX Starter Blog with Gatsby
 
-A simple starter blog using MDX, with [Gatsby](https://www.gatsbyjs.org)
+A simple starter blog using MDX, with [Gatsby](https://www.gatsbyjs.org).
+
+Upgrade to core-js@3
+> npm install --save core-js@^3
+
+To check if your packages are outdated.
+> npm outdated
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,38 @@
 const siteMetadata = {
-    title: `MDX Starter Blog`,
-      description: `This is a simple blog.`,
-  };
-
+  title: `MDX Starter Blog`,
+  description: `This is a simple blog.`
+};
 module.exports = {
-    siteMetadata: siteMetadata,
-    plugins: [
-      {
-        resolve: `gatsby-plugin-mdx`,
-        options: {
-          extensions: [`.mdx`, `.md`],
-        },
-      },
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: `${__dirname}/posts`,
-          name: `posts`,
-        },
-      },
-    ],
-  };
+  siteMetadata: siteMetadata,
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590
+            }
+          }
+        ],
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590
+            }
+          }
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: { path: `${__dirname}/posts`, name: `posts` }
+    }
+  ]
+};
