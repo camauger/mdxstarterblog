46c1f3a6d6dedf2274e6e73bc2d24c73a82ec055
Christian Amauger <christian.amauger@gmail.com>
2020-01-10 12:12:32 -0500

    basic components added (dumb, header, layout)

---
+++ src/components/Dump.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// https://github.com/wesbos/dump
+
+const Dump = props => (
+  <div
+    style={{
+      fontSize: 20,
+      border: '1px solid #efefef',
+      padding: 10,
+      background: 'white',
+    }}>
+    {Object.entries(props).map(([key, val]) => (
+      <pre key={key}>
+        <strong style={{ color: 'white', background: 'red' }}>
+          {key} 💩
+        </strong>
+        {JSON.stringify(val, '', ' ')}
+      </pre>
+    ))}
+  </div>
+);
+
+export default Dump;
